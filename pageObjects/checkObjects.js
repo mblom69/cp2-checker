796663d2d177fc14a0c5293b626ab42719aa154e
mtblom <mtblom69@gmail.com>
2019-05-10 16:42:56 -0600

    pageObjects v1.0

---
+++ pageObjects/checkObjects.js
@@ -0,0 +1,15 @@
+module.exports = {
+    url: 'https://devmountain-qa.github.io/Automation-Basics/build/',
+    elements: {
+            numberField: '[name="evenOddInput"]',
+            objectField: '[name="objectFilterInput"]',
+            stringField:'["nameFilterInput"]',
+            palindromeField:'[name="palindromeInput"]',
+            sum1Field:'[name="sumInput1"]',
+            sum2Field:'[name="sumInput2"]',
+            evenField:'[name="evenResults"]',
+            oddField:'[name="oddResults"]'
+
+
+    }
+}
